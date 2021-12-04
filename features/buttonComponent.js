6858fa7ac7a3491ef3b4c017f20851f63ad8aaa2
Urszuja <urszula.jarych@gmail.com>
2021-12-04 19:00:43 +0100

    add koala

--- features/buttonComponent.js
+++ features/buttonComponent.js
@@ -4,10 +4,19 @@ Component has a light grey background and black text. When hovered, the backgrou
 If animate on hover is true, the button will show an icon of Koala to the right of it
 */
 import './buttonComponentStyles.css';
-
+// div relative koala: absolute
 export default function createButton(label, classStyle, animate) {
+  const koalaButton = document.createElement('div');
+  koalaButton.classList.add('koala-button');
   const button = document.createElement('button');
   button.innerHTML = label;
   button.classList.add(classStyle);
-  return button;
+  button.onClick = () => {
+    console.log('hello');
+  };
+  const koala = document.createElement('img');
+  koala.src = 'features/cute_koala.png';
+  koalaButton.appendChild(button);
+  koalaButton.appendChild(koala);
+  return koalaButton;
 }
