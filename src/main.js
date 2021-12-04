dd1be63010c0e7c81ba239af8905ff2350668f1d
NataliaCichonska <natalia.cichonska1103@gmail.com>
2021-12-04 12:03:49 +0100

    adden some kind of data validation with alerts and prestyled

--- src/main.js
+++ src/main.js
@@ -1,5 +1,6 @@
 import './style.css';
+import './views/quiz-settings/quiz-settings.css';
 import {QuizSettings} from "./views/quiz-settings/quiz-settings";
 
 
-document.querySelector('#app').innerHTML = QuizSettings.showSettings();
+document.querySelector('#app').appendChild(QuizSettings.showSettings());
--- src/views/quiz-settings/quiz-settings.js
+++ src/views/quiz-settings/quiz-settings.js
@@ -1,50 +1,120 @@
-class QuizSettings{
+class QuizSettings {
     quizAbout;
-    questionsNumber;
+    questionsNum;
     questionsType;
-
-    static showSettings(){
-        const settings =document.createElement("div");
-        const form=document.createElement("form");
-        const about=document.createElement("div");
-        const questionsNumberdiv=document.createElement("div");
-        const questionsType=document.createElement("div");
-
-        const buttonCats=document.createElement("input");
-        buttonCats.type="button";
-        const buttonDogs=document.createElement("input");
-        buttonDogs.type="button";
-        about.append("Quiz about:",buttonCats,buttonDogs);
-        // buttonCats.addEventListener("click",function(){
-        //     console.log("dotknięty");
-        // });
-
-        const QuestionsNumber=document.createElement("input");
-        questionsNumberdiv.append("Questions number:",QuestionsNumber);
-        
-        const buttonOpen=document.createElement("input");
-        buttonOpen.type="button";
-        const buttonMultipleChoice=document.createElement("input");
-        buttonMultipleChoice.type="button";
-        questionsType.append("Questions type:",buttonOpen,buttonMultipleChoice);
-
-        const buttonStartQuiz=document.createElement("input");
-        buttonStartQuiz.type="submit";
-
-        form.append(about,questionsNumberdiv,questionsType, buttonStartQuiz);
-        settings.appendChild(form);
-
-        console.log(this.quizAbout,this.questionsType,this.questionsNumber);
-
-
-        form.addEventListener("click",function(){
-            console.log("dotknięty");
+    static createButtonCats() {
+        const buttonCats = document.createElement('input');
+        buttonCats.type = 'button';
+        buttonCats.addEventListener('click', () => {
+            this.quizAbout = 'cats';
         });
-
-        return settings.innerHTML;
+        return buttonCats;
     }
+
+    static createButtonDogs() {
+        const buttonDogs = document.createElement('input');
+        buttonDogs.type = 'button';
+        buttonDogs.addEventListener('click', () => {
+            this.quizAbout="dogs"
+        });
+        return buttonDogs;
+    }
+
+    static createAboutSection() {
+        const about = document.createElement('div');
+        const text = document.createElement('p');
+        text.innerText='Quiz about: '
+        about.append(text, this.createButtonCats(), this.createButtonDogs());
+        return about;
+    }
+
+    static createQuestionsNumberInput() {
+        const questionsNumber = document.createElement('input');
+        questionsNumber.setAttribute('id','number');
+        questionsNumber.addEventListener('input', () => {
+            this.questionsNum=questionsNumber.value;
+        });
+        return questionsNumber;
+    }
+
+    static createQuestionsNumberSection() {
+        const questionsNumberdiv = document.createElement('div');
+        const text = document.createElement('p');
+        text.innerText='Questions number: '
+        questionsNumberdiv.append(text, this.createQuestionsNumberInput());
+        return questionsNumberdiv;
+    }
+
+    static createButtonOpen() {
+        const buttonOpen = document.createElement('input');
+        buttonOpen.type = 'button';
+        buttonOpen.addEventListener('click', () => {
+            this.questionsType="open"
+        });
+        return buttonOpen;
+    }
+
+    static createMultipleChoice() {
+        const buttonMultipleChoice = document.createElement('input');
+        buttonMultipleChoice.type = 'button';
+        buttonMultipleChoice.addEventListener('click', () => {
+            this.questionsType="multiple"
+        });
+        return buttonMultipleChoice;
+    }
+
+    static createQuestionsTypeSection() {
+        const questionsType = document.createElement('div');
+        const text = document.createElement('p');
+        text.innerText='Questions type: '
+        questionsType.append(text, this.createButtonOpen(), this.createMultipleChoice());
+        return questionsType;
+    }
+
+    static createForm() {
+        const form = document.createElement('form');
+
+        const buttonStartQuiz = document.createElement('input');
+        buttonStartQuiz.type = 'submit';
+        buttonStartQuiz.setAttribute('id','submit');
+        buttonStartQuiz.value='Start Quiz';
+
+        form.append(
+            this.createAboutSection(),
+            this.createQuestionsNumberSection(),
+            this.createQuestionsTypeSection(),
+            buttonStartQuiz,
+        );
+        form.addEventListener("submit", e => {
+            e.preventDefault();
+            if (this.questionsNum===undefined || this.questionsNum<1 || this.questionsNum>20){
+                alert("Insert questions number between 1 and 20");
+            }
+            else if (this.quizAbout===undefined){
+                alert("Choose animals");
+            }
+            else if (this.quizAbout===undefined){
+                alert("Choose questionsType");
+            }
+            else {
+                alert("Quiz about: "+this.quizAbout+"\nQuestions number: "+this.questionsNum+"\nQuestions type: "+this.questionsType);
+            }
+
+        });
+        return form;
+    }
+
+    static showSettings() {
+        const settings = document.createElement('div');
+        settings.appendChild(this.createForm());
+        settings.setAttribute('id','quiz-settings');
+
+        console.log(this.quizAbout, this.questionsType, this.questionsNum);
+
+        return settings;
+    }
+}
+
+export {
+    QuizSettings
 };
-
-
-
-export {QuizSettings};
