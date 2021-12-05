87e7e8a890f34057a65bddb4e4419221dc44fc0b
GRosza <grzegorz.roszak@coderscrew.pl>
2021-12-05 20:28:39 +0100

    new submit function

--- src/feature:user-score/user-score.js
+++ src/feature:user-score/user-score.js
@@ -1,15 +1,27 @@
-
 const username = document.getElementById('username');
 const saveScoreBtn = document.getElementById('saveScoreBtn');
-username.addEventListener("keyup", () =>
-{saveScoreBtn.disabled = !username.value;});
 
+username.addEventListener('keyup', () => {
+  saveScoreBtn.disabled = !username.value;
+});
 
+const recentUserScore = '90';
+const finalScore = document.getElementById('finalScore');
+finalScore.innerText = recentUserScore;
 
+const highScores = JSON.parse(localStorage.getItem('highScores')) || [];
+console.log(highScores);
 
 saveHighScore = (e) => {
   console.log('clikedItem');
   e.preventDefault();
+
+  const score = {
+    score: recentUserScore,
+    name: username.value,
+  };
+
+  highScores.push(score);
+  console.log(highScores);
+  localStorage.setItem('highScores', JSON.stringify(highScores));
 };
-
-
