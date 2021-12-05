22550d61d25a8f9b7bfaf64de7b47aac5829329e
Sylwia <mangoweb@wp.pl>
2021-12-05 19:17:57 +0100

    Add main page

--- src/main.js
+++ src/main.js
@@ -1,5 +1,13 @@
 import './style.css';
 
-document.querySelector('#app').innerHTML = `
-  <h1>CC first project</h1>
-`;
+import { renderMainPage } from './views/MainPage/mainPage.js';
+
+function main() {
+  renderMainPage();
+}
+
+main();
+
+// document.querySelector('#app').innerHTML = `
+//   <h1>CC first project</h1>
+// `;
