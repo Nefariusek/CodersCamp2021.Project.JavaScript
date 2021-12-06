561c0bade9de55aad3a537d8cdbaa820e5e90d7e
Urszuja <urszula.jarych@gmail.com>
2021-12-06 10:26:21 +0100

    Move code to the proper folder

--- src/components/Button.js
+++ src/components/Button.js
@@ -3,9 +3,9 @@ Component accepts following inputs: label (string), class (string), animate on h
 Component has a light grey background and black text. When hovered, the background changes to dark grey and the label is white.
 If animate on hover is true, the button will show an icon of Koala to the right of it
 */
-import './buttonComponentStyles.css';
+import './Button.css';
 
-export default function createButton(label, classStyle, animate) {
+export default function Button(label, classStyle, animate) {
   const koalaButton = document.createElement('div');
   koalaButton.classList.add('koala-button');
 
@@ -21,7 +21,7 @@ export default function createButton(label, classStyle, animate) {
 
   if (animate) {
     const koala = document.createElement('img');
-    koala.src = 'features/cute_koala.png';
+    koala.src = '/cute_koala.png';
     koala.classList.add('koala', 'hidden');
     koalaButton.append(koala);
 
--- src/main.js
+++ src/main.js
@@ -1,15 +1,15 @@
 import './style.css';
-import createButton from '../features/buttonComponent.js';
+import Button from './components/Button.js';
 
 document.querySelector('#app').innerHTML = `
   <h1>CC first project</h1>
 `;
-// how to use: createButton(label, classStyle, animate)
-const buttonQuiz = createButton('start quiz', 'quiz', true);
-const buttonLeaderboard = createButton('leaderboard', 'leaderboard', true);
-const buttonAdoption = createButton('adoption', 'adoption', true);
-const buttonCredits = createButton('credits', 'credits', true);
-const buttonStatic = createButton('abort koala', 'noKoala', false);
+// how to use: Button(label, classStyle, animate, listener, callback)
+const buttonQuiz = Button('start quiz', 'quiz', true);
+const buttonLeaderboard = Button('leaderboard', 'leaderboard', true);
+const buttonAdoption = Button('adoption', 'adoption', true);
+const buttonCredits = Button('credits', 'credits', true);
+const buttonStatic = Button('abort koala', 'noKoala', false);
 
 document.querySelector('#app').append(buttonQuiz, buttonLeaderboard, buttonAdoption, buttonCredits);
 document.querySelector('#app').append(buttonStatic);
