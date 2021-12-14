4b9d7b168e43e0dcec06d7c4d1c9b87295eec921
Sylwia <mangoweb@wp.pl>
2021-12-14 19:43:47 +0100

    Fix merge issue

--- src/main.js
+++ src/main.js
@@ -2,11 +2,7 @@ import './style.css';
 import './components/timer/quiz-timer.css';
 import './views/quiz-settings/quiz-settings.css';
 
-<<<<<<< Updated upstream
 import { Router } from './components/router/Router.js';
-=======
-import { renderMainPage } from './views/MainPage/MainPage.js';
->>>>>>> Stashed changes
 
 window.addEventListener('load', Router);
 window.addEventListener('hashchange', Router);
