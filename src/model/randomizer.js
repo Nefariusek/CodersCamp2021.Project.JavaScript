d43424046bcd6ef7e9388f5d129e5ba12da48189
Urszuja <urszula.jarych@gmail.com>
2021-12-17 18:30:50 +0100

    initializing randomizer issue

---
+++ src/model/randomizer.js
@@ -0,0 +1,10 @@
+/*As a user, I want to quiz with the exact number of questions in my quiz. Those questions are random each time I start a new quiz.
+
+Acceptance criteria
+Verify, consult and update before starting.
+
+ Exact number of questions is shown (based on the user's input from Quiz settings);
+ Questions are random each time the quiz is opened;
+Additional notes
+This functionality should be stored in a separate .js file.
+As an input, an array with all retrieved Questions will be given. */
