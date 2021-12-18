382ba588b4eab8bbf1b989cbb96c1da0f21f311e
Sylwia <mangoweb@wp.pl>
2021-12-18 22:55:30 +0100

    Added the ability to randomly download the fact either about dogs or about cats

--- src/api/FactsController.js
+++ src/api/FactsController.js
@@ -1,7 +1,14 @@
 export async function retrieveAnimalFact() {
-  const FACT_URL = `https://cat-fact.herokuapp.com/facts/random?animal_type=cat&amount=1`;
+  const animalTypes = ['dog', 'cat'];
+  const randomAnimalType = randomIntFromInterval(0, animalTypes.length - 1);
+  const animalType = animalTypes[randomAnimalType];
+  const FACT_URL = `https://cat-fact.herokuapp.com/facts/random?animal_type=${animalType}&amount=1`;
 
   const response = await fetch(FACT_URL);
   const json = await response.json();
   return json.text;
+}
+
+function randomIntFromInterval(min, max) {
+  return Math.round(Math.random() * (max - min) + min);
 }
