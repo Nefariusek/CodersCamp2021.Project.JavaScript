c76d4c36f797dd4a10cf6d71c2952dab6b8bbfec
Sylwia <mangoweb@wp.pl>
2021-12-24 00:38:04 +0100

    some fixes in ImageFact

--- src/model/Fact.js
+++ src/model/Fact.js
@@ -1,15 +1,13 @@
-const FactDefaults = {
-  FACT: "Cat's eyes shine in the dark because of the tapetum, a reflective layer in the eye, which acts like a mirror.",
-  IMG_URL: 'http://placekitten.com/400/500',
-};
+const DEFAULT_FACT =
+  "Cat's eyes shine in the dark because of the tapetum, a reflective layer in the eye, which acts like a mirror.";
 
 export default class Fact {
-  _sentence = FactDefaults.FACT;
+  _sentence = DEFAULT_FACT;
 
   get sentence() {
     return this._sentence;
   }
   set sentence(value) {
-    this._sentence = value ? value : FactDefaults.FACT;
+    this._sentence = value ? value : DEFAULT_FACT;
   }
 }
--- src/views/MainPage/MainPage.js
+++ src/views/MainPage/MainPage.js
@@ -1,7 +1,7 @@
 import './MainPage.css';
 import Button from '../../components/Button/Button.js';
 import Bubble from '../../components/Bubble/Bubble.js';
-import ImageFact from '../../model/Fact.js';
+import ImageFact from '../../model/ImageFact.js';
 import { retrieveAnimalFact } from '../../api/FactsController.js';
 import { onNavigationChange } from '../../components/router/Router.js';
 
