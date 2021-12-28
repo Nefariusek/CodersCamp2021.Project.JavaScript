039f32f7c7d44a6bf65e3b96155325231ab70c39
GRosza <grzegorz.roszak@coderscrew.pl>
2021-12-28 22:25:01 +0100

    corrected input

--- src/views/ScorePage/ScorePage.js
+++ src/views/ScorePage/ScorePage.js
@@ -48,7 +48,7 @@ function renderNickForm() {
   nickFormContainer.setAttribute('id', 'nickFormDiv');
   const input = document.createElement('input');
   input.setAttribute('id', 'nickname');
-  input.setAttribute('placeholder', 'NICK');
+  input.setAttribute('placeholder', 'Enter Nickname');
   input.setAttribute('type', 'text');
   input.setAttribute('style', 'text-transform: uppercase');
   nickFormContainer.append(input, Button('SUBMIT', 'saveScoreBtn', false, 'click', saveQuizScore));
