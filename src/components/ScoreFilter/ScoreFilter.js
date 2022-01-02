127691045e53a5c35920cd7c156500a08b0347b9
Urszuja <urszula.jarych@gmail.com>
2022-01-02 15:32:18 +0100

    add range

--- src/components/ScoreFilter/ScoreFilter.js
+++ src/components/ScoreFilter/ScoreFilter.js
@@ -4,15 +4,51 @@ export default function ScoreFilter() {
   const scoreFilter = document.createElement('div');
   scoreFilter.classList.add('score-filter');
 
+  const dropdown = Dropdown();
+  scoreFilter.appendChild(dropdown);
+
+  const questionRange = RangeSelect();
+  scoreFilter.appendChild(questionRange);
+
+  return scoreFilter;
+}
+
+function RangeSelect() {
+  const range = document.createElement('div');
+  range.classList.add('score-range');
+
+  const label = document.createElement('label');
+  label.innerHTML = 'Question number range';
+
+  const labelMin = document.createElement('p');
+  labelMin.innerHTML = 'Min';
+
+  const inputMin = document.createElement('input');
+  inputMin.type = 'number';
+
+  const labelMax = document.createElement('p');
+  labelMax.innerHTML = 'Max';
+  const inputMax = document.createElement('input');
+  inputMax.type = 'number';
+
+  range.append(label, labelMin, inputMin, labelMax, inputMax);
+
+  return range;
+}
+
+function Dropdown() {
+  const dropdown = document.createElement('div');
+  dropdown.classList.add('dropdown');
+
   const label = document.createElement('label');
   label.innerHTML = 'Filter results by:';
   label.setAttribute('for', 'settings');
 
-  scoreFilter.appendChild(label);
+  dropdown.appendChild(label);
 
-  const dropdown = document.createElement('select');
-  dropdown.setAttribute('name', 'settings');
-  dropdown.setAttribute('id', 'settings');
+  const select = document.createElement('select');
+  select.setAttribute('name', 'settings');
+  select.setAttribute('id', 'settings');
 
   const typeSetting = document.createElement('option');
   typeSetting.setAttribute('value', 'type-setting');
@@ -22,18 +58,9 @@ export default function ScoreFilter() {
   aboutSetting.setAttribute('value', 'about-setting');
   aboutSetting.innerHTML = 'About';
 
-  dropdown.append(typeSetting, aboutSetting);
-  scoreFilter.appendChild(dropdown);
-  /*
-<label for="cars">Choose a car:</label>
+  select.append(typeSetting, aboutSetting);
 
-<select name="cars" id="cars">
-  <option value="volvo">Volvo</option>
-  <option value="saab">Saab</option>
-  <option value="mercedes">Mercedes</option>
-  <option value="audi">Audi</option>
-</select>
-*/
+  dropdown.append(select);
 
-  return scoreFilter;
+  return dropdown;
 }
